3e907eed91418a6f81f6bb98fd929f73f510f9f6
ruygomes <ruy.gomes@gmail.com>
2020-03-31 21:44:43 -0400

    criados atributos de nome e idade

--- oo/pessoa.py
+++ oo/pessoa.py
@@ -1,4 +1,8 @@
 class Pessoa:
+    def __init__(self, nome=None, idade=35):
+        self.idade = idade
+        self.nome = nome
+
     def cumprimentar(self):
         return f'Olá {id(self)}'
 
@@ -7,3 +11,7 @@ if __name__ == '__main__':
     print(Pessoa.cumprimentar(p))
     print(id(p))
     print(p.cumprimentar())
+    print(p.nome)
+    p.nome = 'Ruy Gomes'
+    print(p.nome)
+    print(p.idade)
